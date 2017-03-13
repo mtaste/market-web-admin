b1ffc8c661a58ddcc8540b538c589352c4c28092
ken <feng36201@126.com>
2017-03-13 18:42:22 +0800

    Signed-off-by: ken <feng36201@126.com>

--- src/main/java/com/app/market/web/admin/controller/agent/AgentInfoController.java
+++ src/main/java/com/app/market/web/admin/controller/agent/AgentInfoController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.app.market.dto.agent.AgentPointsChangeDTO;
+import com.app.market.dto.agent.AgentPointsChangeDetailDTO;
 import com.app.market.dto.common.PageBean;
 import com.app.market.dto.common.PageDTO;
 import com.app.market.dto.user.SysUserDTO;
@@ -68,4 +70,183 @@ public class AgentInfoController {
 		return ret;
 	}
 
+	// =================================积分变更================================
+	/**
+	 * 获取列表
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("points-change/list")
+	@ResponseBody
+	public Object pointsChangeList(String param, HttpServletRequest request) {
+		logger.info("points-change.list");
+		Result ret = new Result();
+		PageDTO page = JsonUtil.parse(param, PageDTO.class);
+		AgentPointsChangeDTO p = JsonUtil.parse(param, AgentPointsChangeDTO.class);
+		String userId = Request.getUserId(request);
+		p.setUpdateUser(userId);
+		PageBean<Map<String, String>> r = this.agentService.getPointsChangeList(page, p);
+		ret.setData(r);
+		return ret;
+	}
+
+	/**
+	 * 保存定义
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("points-change/save")
+	@ResponseBody
+	public Object pointsChangeSave(String param, HttpServletRequest request) {
+		logger.info("org.save");
+		Result ret = new Result();
+		AgentPointsChangeDTO p = JsonUtil.parse(param, AgentPointsChangeDTO.class);
+		String userId = Request.getUserId(request);
+		p.setUpdateUser(userId);
+		String r = this.agentService.savePointsChangeData(p);
+		ret.setData(r);
+		return ret;
+	}
+
+	/**
+	 * 删除
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("points-change/remove")
+	@ResponseBody
+	public Object pointsChangeRemove(String param, HttpServletRequest request) {
+		logger.info("points-change.remove");
+		Result ret = new Result();
+		AgentPointsChangeDTO p = JsonUtil.parse(param, AgentPointsChangeDTO.class);
+		String r = this.agentService.removePointsChangeData(p);
+		ret.setData(r);
+		return ret;
+	}
+
+	/**
+	 * 提交
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("points-change/app")
+	@ResponseBody
+	public Object pointsChangeApp(String param, HttpServletRequest request) {
+		logger.info("points-change.app");
+		Result ret = new Result();
+		AgentPointsChangeDTO p = JsonUtil.parse(param, AgentPointsChangeDTO.class);
+		String userId = Request.getUserId(request);
+		p.setUpdateUser(userId);
+		String r = this.agentService.appPointsChangeData(p);
+		ret.setData(r);
+		return ret;
+	}
+
+	/**
+	 * 获取明细列表
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("points-change/detail")
+	@ResponseBody
+	public Object changeDetail(String param, HttpServletRequest request) {
+		logger.info("points-change.detail");
+		Result ret = new Result();
+		PageDTO page = JsonUtil.parse(param, PageDTO.class);
+		AgentPointsChangeDTO p = JsonUtil.parse(param, AgentPointsChangeDTO.class);
+		String userId = Request.getUserId(request);
+		p.setUpdateUser(userId);
+		PageBean<Map<String, String>> r = this.agentService.pointsChangeDetail(page, p);
+		ret.setData(r);
+		return ret;
+	}
+
+	/**
+	 * 获取产品明细列表
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("points-change/memberList")
+	@ResponseBody
+	public Object pointsChangeMemberList(String param, HttpServletRequest request) {
+		logger.info("points-change.memberList");
+		Result ret = new Result();
+		PageDTO page = JsonUtil.parse(param, PageDTO.class);
+		AgentPointsChangeDTO p = JsonUtil.parse(param, AgentPointsChangeDTO.class);
+		String userId = Request.getUserId(request);
+		p.setUpdateUser(userId);
+		PageBean<Map<String, String>> r = this.agentService.getPointsChangeMemberList(page, p);
+		ret.setData(r);
+		return ret;
+	}
+
+	/**
+	 * 修改明细数量
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("points-change/detailPoints")
+	@ResponseBody
+	public Object pointsChangeDetailPoints(String param, HttpServletRequest request) {
+		logger.info("points-change.detailPoints");
+		Result ret = new Result();
+		AgentPointsChangeDetailDTO p = JsonUtil.parse(param, AgentPointsChangeDetailDTO.class);
+		String userId = Request.getUserId(request);
+		p.setUpdateUser(userId);
+		String r = this.agentService.addPointsChangeDetailPoints(p);
+		ret.setData(r);
+		return ret;
+	}
+
+	/**
+	 * 删除定义
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("points-change/removeDetail")
+	@ResponseBody
+	public Object changeRemoveDetail(String param, HttpServletRequest request) {
+		logger.info("points-change.removeDetail");
+		Result ret = new Result();
+		AgentPointsChangeDTO p = JsonUtil.parse(param, AgentPointsChangeDTO.class);
+		String r = this.agentService.removePointsChangeDetail(p);
+		ret.setData(r);
+		return ret;
+	}
+
+	/**
+	 * 删除定义
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("points-change/addDetail")
+	@ResponseBody
+	public Object changeAddDetail(String param, HttpServletRequest request) {
+		logger.info("points-change.addDetail");
+		Result ret = new Result();
+		AgentPointsChangeDTO p = JsonUtil.parse(param, AgentPointsChangeDTO.class);
+		String userId = Request.getUserId(request);
+		p.setUpdateUser(userId);
+		String r = this.agentService.addPointsChangeDetail(p);
+		ret.setData(r);
+		return ret;
+	}
 }
