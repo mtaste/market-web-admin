981a8627c8cb1e45de95a3a30b399e407a749959
ken <feng36201@126.com>
2017-03-29 11:49:11 +0800

    Signed-off-by: ken <feng36201@126.com>

---
+++ src/main/java/com/app/market/web/admin/controller/asset/AssetManageController.java
@@ -0,0 +1,116 @@
+package com.app.market.web.admin.controller.asset;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.app.market.dto.asset.AssetManageDTO;
+import com.app.market.dto.asset.AssetRegisterDTO;
+import com.app.market.dto.common.PageBean;
+import com.app.market.dto.common.PageDTO;
+import com.app.market.dto.user.SysUserDTO;
+import com.app.market.service.asset.AssetManageService;
+import com.app.market.support.dto.Result;
+import com.app.market.support.util.JsonUtil;
+import com.app.market.support.util.Request;
+import com.app.market.support.util.Version;
+import com.app.market.web.admin.controller.agent.AgentInfoController;
+
+@Controller
+@RequestMapping("/asset/manage/")
+public class AssetManageController {
+	private static final Logger logger = LoggerFactory.getLogger(AgentInfoController.class);
+	@Reference(version = Version.NOW)
+	private AssetManageService assetManageService;
+
+	/**
+	 * 获取菜单功能
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("list")
+	@ResponseBody
+	public Object list(String param, HttpServletRequest request) {
+		logger.info("list");
+		Result ret = new Result();
+		PageDTO page = JsonUtil.parse(param, PageDTO.class);
+		AssetRegisterDTO p = JsonUtil.parse(param, AssetRegisterDTO.class);
+		String userId = Request.getUserId(request);
+		p.setUpdateUser(userId);
+		PageBean<Map<String, String>> r = this.assetManageService.getAssetList(p, page);
+		ret.setData(r);
+		return ret;
+	}
+
+	/**
+	 * 选择用户
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("user-choose")
+	@ResponseBody
+	public Object userChoose(String param, HttpServletRequest request) {
+		logger.info("list");
+		Result ret = new Result();
+		PageDTO page = JsonUtil.parse(param, PageDTO.class);
+		SysUserDTO p = JsonUtil.parse(param, SysUserDTO.class);
+		String userId = Request.getUserId(request);
+		p.setUpdateUser(userId);
+		PageBean<Map<String, String>> r = this.assetManageService.getAssetUserChooseList(p, page);
+		ret.setData(r);
+		return ret;
+	}
+
+	/**
+	 * 借出
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("detail")
+	@ResponseBody
+	public Object detail(String param, HttpServletRequest request) {
+		logger.info("detail");
+		Result ret = new Result();
+		PageDTO page = JsonUtil.parse(param, PageDTO.class);
+		AssetRegisterDTO p = JsonUtil.parse(param, AssetRegisterDTO.class);
+		String userId = Request.getUserId(request);
+		p.setUpdateUser(userId);
+		PageBean<Map<String, String>> r = this.assetManageService.getAssetDetailList(p, page);
+		ret.setData(r);
+		return ret;
+	}
+
+	/**
+	 * 借出
+	 * 
+	 * @param param
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("loan")
+	@ResponseBody
+	public Object loan(String param, HttpServletRequest request) {
+		logger.info("loan");
+		Result ret = new Result();
+		AssetManageDTO p = JsonUtil.parse(param, AssetManageDTO.class);
+		String userId = Request.getUserId(request);
+		p.setUpdateUser(userId);
+		String r = this.assetManageService.assetLoan(p);
+		ret.setData(r);
+		return ret;
+	}
+
+}
